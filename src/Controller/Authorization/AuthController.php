e74c0b17009b4c7d5ef33582ce2834b3d9a0e08e
baktybekovich1 <kydyrmysheva2005@gmail.com>
2025-12-08 21:47:03 +0600

    Telegram auth fix

--- src/Controller/Authorization/AuthController.php
+++ src/Controller/Authorization/AuthController.php
@@ -25,12 +25,15 @@ class AuthController extends AbstractController
     {
     }
 
-    #[Route('/api/telegram/webapp', name: 'telegram_webapp', methods: ['POST'])]
-    public function webApp(Request $request): JsonResponse
-    {
-        $data = json_decode(base64_decode($request->request->get('tgAuthResult')), true);
+    #[Route('/api/telegram/check', name: 'telegram_check')]
+    public function check(
+        Request $request,
+
+    ): JsonResponse {
+        $data = $request->query->all();
         if (!$this->validateTelegramAuth($data)) {
-            return new JsonResponse(['error' => 'invalid'], 403);
+            return $this->json(['token' => false]);
+//            return $this->redirect('https://yourfrontend.com/auth/callback?error=invalid');
         }
 
         $user = $this->userRepository->findOneBy(['telegramId' => $data['id']]);
@@ -52,6 +55,44 @@ class AuthController extends AbstractController
         $hash = $data['hash'] ?? '';
         unset($data['hash']);
 
+        $checkArr = [];
+        foreach ($data as $k => $v) {
+            $checkArr[] = $k . '=' . $v;
+        }
+        sort($checkArr);
+        $secret = hash('sha256', $_ENV['TELEGRAM_BOT_TOKEN'], true);
+        $calcHash = hash_hmac('sha256', implode("\n", $checkArr), $secret);
+
+        return hash_equals($calcHash, $hash);
+    }
+
+    #[Route('/api/telegram/webapp', name: 'telegram_webapp', methods: ['POST'])]
+    public function webApp(Request $request): JsonResponse
+    {
+        $data = json_decode(base64_decode($request->request->get('tgAuthResult')), true);
+        if (!$this->validateTelegramAut($data)) {
+            return new JsonResponse(['error' => 'invalid'], 403);
+        }
+
+        $user = $this->userRepository->findOneBy(['telegramId' => $data['id']]);
+        if (!$user) {
+            $user = new User();
+            $user->setTelegramId((int)$data['id']);
+            $user->setUsername($data['username'] ?? 'user_' . $data['id']);
+            $this->em->persist($user);
+            $this->em->flush();
+        }
+
+        $token = $this->jwtManager->create($user);
+
+        return $this->json(['token'=> $token]);
+    }
+
+    private function validateTelegramAut(array $data): bool
+    {
+        $hash = $data['hash'] ?? '';
+        unset($data['hash']);
+
         // удаляем поля, которые Telegram НЕ подписывает
         unset($data['v']);          // версия WebApp (если есть)
         unset($data['bot_id']);     // тоже не участвует
@@ -68,5 +109,6 @@ class AuthController extends AbstractController
 
         return hash_equals($calcHash, $hash);
     }
+
 
 }
