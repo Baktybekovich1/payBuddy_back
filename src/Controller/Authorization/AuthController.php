da0f5b6489a3f50fc8c51522371337e451bd3340
baktybekovich1 <kydyrmysheva2005@gmail.com>
2025-12-08 20:52:41 +0600

    Telegram auth

---
+++ src/Controller/Authorization/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller\Authorization;
+
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthController extends AbstractController
+{
+    public function __construct(
+
+        private readonly UserRepository $userRepository,
+        private readonly EntityManagerInterface $em,
+        private readonly JWTTokenManagerInterface $jwtManager
+    )
+    {
+    }
+
+    #[Route('/api/telegram/check', name: 'telegram_check')]
+    public function check(
+        Request $request,
+
+    ): JsonResponse {
+        $data = $request->query->all();
+        if (!$this->validateTelegramAuth($data)) {
+            return $this->json(['token' => false]);
+//            return $this->redirect('https://yourfrontend.com/auth/callback?error=invalid');
+        }
+
+        $user = $this->userRepository->findOneBy(['telegramId' => $data['id']]);
+        if (!$user) {
+            $user = new User();
+            $user->setTelegramId((int)$data['id']);
+            $user->setUsername($data['username'] ?? 'user_' . $data['id']);
+            $this->em->persist($user);
+            $this->em->flush();
+        }
+
+        $token = $this->jwtManager->create($user);
+
+        return $this->json(['token'=> $token]);
+    }
+
+    private function validateTelegramAuth(array $data): bool
+    {
+        $hash = $data['hash'] ?? '';
+        unset($data['hash']);
+
+        $checkArr = [];
+        foreach ($data as $k => $v) {
+            $checkArr[] = $k . '=' . $v;
+        }
+        sort($checkArr);
+        $secret = hash('sha256', $_ENV['TELEGRAM_BOT_TOKEN'], true);
+        $calcHash = hash_hmac('sha256', implode("\n", $checkArr), $secret);
+
+        return hash_equals($calcHash, $hash);
+    }
+
+}
